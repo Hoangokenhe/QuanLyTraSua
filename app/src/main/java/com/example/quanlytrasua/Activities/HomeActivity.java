ce4b4e34d912501c070f6ecba47c07b50a1d95ea
manhdepzai <manhbadboys@gmail.com>
2023-04-01 09:28:53 +0700

    dang nhap dang ky

---
+++ app/src/main/java/com/example/quanlytrasua/Activities/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.quanlytrasua.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.example.quanlytrasua.R;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+
+
+    }}
