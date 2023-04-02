58684c96637e923c157db9428a6922664310f323
pc <hoangja12@gmail.com>
2023-04-02 20:28:38 +0700

    Hoang Bill Lương

---
+++ app/src/main/java/com/example/quanlytrasua/Activities/StaticThongKe.java
@@ -0,0 +1,11 @@
+package com.example.quanlytrasua.Activities;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StaticThongKe extends AppCompatActivity {
+
+
+
+}
