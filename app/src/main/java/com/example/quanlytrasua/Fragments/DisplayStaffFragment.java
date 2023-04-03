5147c1808185fd3858297ae682dc4f83e1d99be0
Admin <dangthehao1412@gmail.com>
2023-04-03 23:47:42 +0700

    fragmentstaffand active

--- app/src/main/java/com/example/quanlytrasua/Fragments/DisplayStaffFragment.java
+++ app/src/main/java/com/example/quanlytrasua/Fragments/DisplayStaffFragment.java
@@ -18,18 +18,145 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.quanlytrasua.Activities.AddStaffActivity;
+import com.example.quanlytrasua.Activities.HomeActivity;
+import com.example.quanlytrasua.Activities.SetLuongActiviy;
+import com.example.quanlytrasua.CustomAdapter.AdapterDisplayStaff;
+import com.example.quanlytrasua.DAO.NhanVienDAO;
+import com.example.quanlytrasua.DTO.NhanVienDTO;
 import com.example.quanlytrasua.R;
+
 
 import java.util.List;
 
-
 public class DisplayStaffFragment extends Fragment {
 
+    GridView gvStaff;
+    NhanVienDAO nhanVienDAO;
+    List<NhanVienDTO> nhanVienDTOS;
+    AdapterDisplayStaff adapterDisplayStaff;
+
+    ActivityResultLauncher<Intent> resultLauncherAdd = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent intent = result.getData();
+                        long ktra = intent.getLongExtra("ketquaktra",0);
+                        String chucnang = intent.getStringExtra("chucnang");
+                        if(chucnang.equals("themnv"))
+                        {
+                            if(ktra != 0){
+                                HienThiDSNV();
+                                Toast.makeText(getActivity(),"Thêm thành công",Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(getActivity(),"Thêm thất bại",Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+                            if(ktra != 0){
+                                HienThiDSNV();
+                                Toast.makeText(getActivity(),"Sửa thành công",Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(getActivity(),"Sửa thất bại",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    }
+                }
+            });
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.displaystaff_layout, container, false);
+        View view = inflater.inflate(R.layout.displaystaff_layout,container,false);
+        ((HomeActivity)getActivity()).getSupportActionBar().setTitle("Quản lý nhân viên");
+        setHasOptionsMenu(true);
+
+        gvStaff = (GridView)view.findViewById(R.id.gvStaff) ;
+
+        nhanVienDAO = new NhanVienDAO(getActivity());
+        HienThiDSNV();
+
+        registerForContextMenu(gvStaff);
+        gvStaff.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int maNhanVien = nhanVienDTOS.get(position).getMANV();
+                if(maNhanVien == 1){
+                    Toast.makeText(getActivity(),"Admin không cần set lương",Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(getActivity(), SetLuongActiviy.class);
+                    intent.putExtra("MaNhanVien",maNhanVien);
+                    startActivity(intent);
+                }
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu,View v,ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getActivity().getMenuInflater().inflate(R.menu.edit_context_menu,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int vitri = menuInfo.position;
+        int manv = nhanVienDTOS.get(vitri).getMANV();
+
+        switch (id){
+            case R.id.itEdit:
+                Intent iEdit = new Intent(getActivity(), AddStaffActivity.class);
+                iEdit.putExtra("manv",manv);
+                resultLauncherAdd.launch(iEdit);
+                break;
+
+            case R.id.itDelete:
+                boolean ktra = nhanVienDAO.XoaNV(manv);
+                if(ktra){
+                    HienThiDSNV();
+                    Toast.makeText(getActivity(),getActivity().getResources().getString(R.string.delete_sucessful)
+                            ,Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getActivity(),getActivity().getResources().getString(R.string.delete_failed)
+                            ,Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        MenuItem itAddStaff = menu.add(1,R.id.itAddStaff,1,"Thêm nhân viên");
+        itAddStaff.setIcon(R.drawable.ic_baseline_add_24);
+        itAddStaff.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.itAddStaff:
+                Intent iDangky = new Intent(getActivity(), AddStaffActivity.class);
+                resultLauncherAdd.launch(iDangky);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void HienThiDSNV(){
+        nhanVienDTOS = nhanVienDAO.LayDSNV();
+        adapterDisplayStaff = new AdapterDisplayStaff(getActivity(),R.layout.custom_layout_displaystaff,nhanVienDTOS);
+        gvStaff.setAdapter(adapterDisplayStaff);
+        adapterDisplayStaff.notifyDataSetChanged();
     }
 }
